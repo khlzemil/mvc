a4d982af05de41f82182a46b0191d4f94c013abd
khlzemil <emilmg@code.edu.az>
2022-10-05 15:19:39 +0400

    mvc_about

---
+++ front-to-back/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using front_to_back.Models;
+using front_to_back.ViewModels.About;
+using Microsoft.AspNetCore.Mvc;
+
+namespace front_to_back.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projectComponents = new List<ProjectComponent>
+            {
+                new ProjectComponent{Title = "Our vision", Text = "Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra."},
+                new ProjectComponent{Title = "Our mission", Text = "Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis."},
+                new ProjectComponent{Title = "Our goal", Text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor."},
+            };
+
+            var model = new AboutIndexViewModel
+            {
+                ProjectComponents = projectComponents
+            };
+
+            return View(model);
+        }
+    }
+}
